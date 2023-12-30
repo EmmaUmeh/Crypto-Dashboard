2d07dc100761bd2dc3ef4da97e644926fa9358c8
Emmanuel Umeh <emmachinemerem04@gmail.com>
2023-12-30 09:49:00 -0800

    Made_changes

--- components/Barchart/Barchat.tsx
+++ components/Barchart/Barchat.tsx
@@ -14,7 +14,7 @@ const ICOTokenBarchart: React.FC = () => {
     return (
         <React.Fragment>
             <div className="text-sm text-[#B0B1B4]">
-                <BarChart width={570} height={350} data={barchart_data} className="lg:max-w-[900px] max-w-96">
+                <BarChart width={570} height={350} data={barchart_data} className="lg:max-w-[900px] max-w-80">
                     <CartesianGrid strokeDasharray="3 3 3" vertical={false} />
                     <XAxis dataKey="name" axisLine={false} tickLine={false} />
                     <YAxis
