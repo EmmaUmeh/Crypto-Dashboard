9e4a27051d37896fb780e5cc06e9b07baf6c0df3
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-01-01 12:36:59 -0800

    Added_Translate_TO_sideNav

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="lg:pl-[220px] flex justify-center items-center lg:pt-7 lg:p-32">
+    <main className=" flex justify-center items-center lg:pt-7 lg:p-32">
           <Dashboard />
     </main>
   )
--- components/Dashboard/dashboard.tsx
+++ components/Dashboard/dashboard.tsx
@@ -8,16 +8,50 @@ import YuchainBalance from "../YuchainTokenBalance/YuchainBalance";
 const Dashboard: React.FC = () => {
     return (
       <>
-        <div className="relative top-32">
+        <div className="lg:pl-28 lg:pr-5 relative top-32">
           <div className="lg:flex lg:justify-between justify-center gap-52">
-            <div className="lg:flex lg:flex-col text-[#555764] w-50">
-              <span className="mb-8 lg:ml-10">YUCHAIN TOKEN (SUPPLY & DEMAND)</span>
+            <div className="lg:flex lg:flex-col text-[#555764] w-20">
+              <span className="mb-8 lg:ml-10 text-nowrap">YUCHAIN TOKEN (SUPPLY & DEMAND)</span>
               <ICOTokenBarchart />
             </div>
   
-            <div className="lg:flex lg:flex-col w-60 lg:grid-cols-4 lg:justify-self-end">
+            <div className="lg:flex lg:flex-col w-50 lg:grid-cols-4 lg:justify-self-end">
               <span className="mb-8">TOKEN DISTRIBUTION</span>
               <ICOPieChart />
+
+              <div className="mt-7">
+                <ul className="lg:flex items-center text-nowrap gap-8 text-sm text-[#555764]">
+                    <div>
+                    <li className="flex gap-5 justify-between">
+                    Crowd Sales
+                    <span>14%</span>
+                    </li>
+                    <li className="flex gap-5 justify-between">
+                      Team
+                    <span>14%</span>
+                    </li >
+                    <li className="flex gap-5 justify-between">
+                      Advisors
+                      <span>14%</span>
+                    </li>
+                    </div>
+
+                    <div>
+                    <li className="flex justify-between gap-5">
+                    Crowd Sales
+                    <span>14%</span>
+                    </li>
+                    <li className="flex gap-5 justify-between">
+                      Team
+                    <span>14%</span>
+                    </li >
+                    <li className="flex gap-5 justify-between">
+                      Advisors
+                      <span>14%</span>
+                    </li>
+                    </div>
+                </ul>
+              </div>
             </div>
           </div>
 
