94b049b5bf8f1da9f3cd5dd87b3ef3b1544001fa
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-01-01 11:25:18 -0800

    Added_Translate_TO_sideNav

--- components/responsiveNavbar/responsiveNavbar.tsx
+++ components/responsiveNavbar/responsiveNavbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { RiStackLine } from "react-icons/ri";
 import { SlWallet } from "react-icons/sl";
@@ -7,84 +9,84 @@ import { FaQuestion } from "react-icons/fa";
 import { RiUserFollowLine } from "react-icons/ri";
 import Link from "next/link";
 
-
 const ResponsiveNavbar: React.FC = () => {
-    return(
-        <>
-            <div className="z-50">
-            <aside className="lg:hidden h-full mt-4 block bg-[#24303F] fixed left-0 py-8 px-10 pl-3 w-64 flex flex-col transition-all ease-in-out duration-100 transform">
-         
-                <div className="">
-                    <ul >
-                       <Link href={"/"}>
-                            <li className="mb-10 flex items-center gap-3 cursor-pointer">
-                                <LuLayoutDashboard color="#FFF" size="27"/>
-                                <div className="text-sm text-white">
-                                    DASHBOARD
-                                </div>
-                                    
-                                </li>
-                       </Link>
+  const [sidebarVisible, setSidebarVisible] = useState(true);
 
-                        
-                       <Link href={""}>
-                            <li className="mb-10 flex items-center gap-3 cursor-pointer">
-                            <RiStackLine  color="#FFF" size="30"/>
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
 
-                            <div className="text-sm text-white">
-                                    BUY YUCHAIN
-                                </div>
-    
-                                </li>
-                       </Link>
+  return (
+    <>
+      <div className="z-50">
+        <aside
+          className={`lg:hidden h-full mt-4 block bg-[#24303F] fixed left-0 py-8 px-10 pl-3 w-64 flex flex-col transition-all ease-in-out duration-100 transform ${
+            sidebarVisible ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="">
+            <ul>
+              <Link href={"/"}>
+                <li
+                  className="mb-10 flex items-center gap-3 cursor-pointer"
+                  onClick={toggleSidebar}
+                >
+                  <LuLayoutDashboard color="#FFF" size="27" />
+                  <div className="text-sm text-white">DASHBOARD</div>
+                </li>
+              </Link>
 
-                       <Link href={""}>
+              <Link href={""}>
+                <li
+                  className="mb-10 flex items-center gap-3 cursor-pointer"
+                  onClick={toggleSidebar}
+                >
+                  <RiStackLine color="#FFF" size="30" />
+                  <div className="text-sm text-white">BUY YUCHAIN</div>
+                </li>
+              </Link>
 
-                            <li className="mb-10  flex items-center gap-3 cursor-pointer">
-                                    <SlWallet color="#FFF" size="30"/>
-                                    <div className="text-sm text-white">
-                                        WALLET
-                                    </div>
+              <Link href={""}>
+                <li
+                  className="mb-10  flex items-center gap-3 cursor-pointer"
+                  onClick={toggleSidebar}
+                >
+                  <SlWallet color="#FFF" size="30" />
+                  <div className="text-sm text-white">WALLET</div>
+                </li>
+              </Link>
 
-                                </li>
-                       </Link>
+              <Link href={""}>
+                <li
+                  className="mb-10  flex items-center gap-3 cursor-pointer"
+                  onClick={toggleSidebar}
+                >
+                  <BiTransfer color="#FFF" size="30" />
+                  <div className="text-sm text-white">TRANSACTION</div>
+                </li>
+              </Link>
 
-                        <Link href={""}>
-                                <li className="mb-10  flex items-center gap-3 cursor-pointer">
-                                
-                                    <BiTransfer color="#FFF" size="30"/>
-                                    <div className="text-sm text-white">
-                                        WALLET
-                                    </div>
-                                </li>
-                        </Link>
+              <li
+                className="mb-10 flex items-center gap-3 cursor-pointer"
+                onClick={toggleSidebar}
+              >
+                <FaQuestion color="#FFF" size="30" />
+                <div className="text-sm text-white">FAQ</div>
+              </li>
 
-
-                        <li className="mb-10 flex items-center gap-3 cursor-pointer">
-                        
-                             <FaQuestion color="#FFF" size="30"/>
-                             <div className="text-sm text-white">
-                                        FAQ
-                                    </div>
-                        </li>
-
-
-                        
-                        <li className="mb-4 flex items-center gap-3 cursor-pointer">
-                       
-
-                             <RiUserFollowLine color="#FFF" size="30"/>
-                             <div className="text-sm text-white">
-                                     ACCOUNT
-                                    </div>
-                        </li>
-
-                    </ul>
-                </div>
-            </aside>
-            </div>
-        </>
-    )
-}
+              <li
+                className="mb-4 flex items-center gap-3 cursor-pointer"
+                onClick={toggleSidebar}
+              >
+                <RiUserFollowLine color="#FFF" size="30" />
+                <div className="text-sm text-white">ACCOUNT</div>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </div>
+    </>
+  );
+};
 
 export default ResponsiveNavbar;
